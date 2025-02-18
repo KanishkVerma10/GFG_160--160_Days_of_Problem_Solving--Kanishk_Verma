8ce8e44a8dda732923de43eed0573b518923bd9d
KanishkVerma10 <153986381+KanishkVerma10@users.noreply.github.com>
2025-02-18 23:55:03 +0530

    Add files via upload

---
+++ 01_Arrays/01_secondLargest.cpp
@@ -0,0 +1,35 @@
+// Given an array of positive integers arr[], return the second largest element from the array. If the second largest element doesn't exist then return -1.
+
+// Note: The second largest element should not be equal to the largest element.
+
+// Examples:
+
+// Input: arr[] = [12, 35, 1, 10, 34, 1]
+// Output: 34
+// Explanation: The largest element of the array is 35 and the second largest element is 34.
+// Input: arr[] = [10, 5, 10]
+// Output: 5
+// Explanation: The largest element of the array is 10 and the second largest element is 5.
+
+class Solution {
+    public:
+      // Function returns the second
+      // largest elements
+      int getSecondLargest(vector<int> &arr){
+          if(arr.size() < 2) return -1;    
+          
+          int largest = INT_MIN;
+          int secondLargest = INT_MIN;
+          
+          for(int num : arr){
+              if(num > largest){
+                  secondLargest = largest;
+                  largest = num;
+              }
+              else if(num > secondLargest && num < largest){
+                  secondLargest = num;
+              }
+          }
+          return (secondLargest == INT_MIN) ? -1 : secondLargest;
+      }
+  };
