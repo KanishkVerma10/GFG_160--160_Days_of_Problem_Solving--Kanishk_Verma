44ce5550a53a3f7ecf389b4aaca650410567d85c
KanishkVerma10 <153986381+KanishkVerma10@users.noreply.github.com>
2025-02-20 23:55:43 +0530

    Update 04_rotateArray.cpp

--- 01_Arrays/04_rotateArray.cpp
+++ 01_Arrays/04_rotateArray.cpp
@@ -1,7 +1,6 @@
 // Rotate Array
 
-// Given an array arr[]. Rotate the array to the left (counter-clockwise direction) by d steps, where d is a positive integer. Do the mentioned 
-// change in the array in place.
+// Given an array arr[]. Rotate the array to the left (counter-clockwise direction) by d steps, where d is a positive integer. Do the mentioned change in the array in place.
 
 // Note: Consider the array as circular.
 
