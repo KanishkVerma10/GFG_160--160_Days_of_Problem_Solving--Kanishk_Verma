40dcc58b89e2299e99c4fe8612d96bac02618d5f
KanishkVerma10 <153986381+KanishkVerma10@users.noreply.github.com>
2025-02-26 23:59:41 +0530

    Create 10_KadaneAlgorithm-MaximumSumSubarray.cpp

---
+++ 01_Arrays/10_KadaneAlgorithm-MaximumSumSubarray.cpp
@@ -0,0 +1,29 @@
+// Kadane's Algorithm
+
+// Given an integer array arr[]. You need to find the maximum sum of a subarray.
+
+// Examples:
+
+// Input: arr[] = [2, 3, -8, 7, -1, 2, 3]
+// Output: 11
+// Explanation: The subarray {7, -1, 2, 3} has the largest sum 11.
+// Input: arr[] = [-2, -4]
+// Output: -2
+// Explanation: The subarray {-2} has the largest sum -2.
+
+int maxSubarraySum(vector<int> &arr) {
+    int res = arr[0];
+    int maxEnding = arr[0];
+
+    for (int i = 1; i < arr.size(); i++) {
+      
+        // Find the maximum sum ending at index i by either extending 
+        // the maximum sum subarray ending at index i - 1 or by
+        // starting a new subarray from index i
+        maxEnding = max(maxEnding + arr[i], arr[i]);
+      
+        // Update res if maximum subarray sum ending at index i > res
+        res = max(res, maxEnding);
+    }
+    return res;
+}
