758b3263f37e01f5592e9dd66d81aca2f91d837f
KanishkVerma10 <153986381+KanishkVerma10@users.noreply.github.com>
2025-04-03 23:21:01 +0530

    Update 36_SpiralTraversal.cpp

--- 05_Matrix/36_SpiralTraversal.cpp
+++ 05_Matrix/36_SpiralTraversal.cpp
@@ -2,7 +2,7 @@
 
 // You are given a rectangular matrix mat[][] of size n x m, and your task is to return an array while traversing the matrix in spiral form.
 
-vector<int> spirallyTraverse(vector<vector<int> > &mat) {
+vector<int> spirallyTraverse(vector<vector<int> > &mat){
     int m = mat.size();
     int n = mat[0].size();
     
